6ed298d54eb6fafa9f9693a1f714460e1d5736aa
vvsisaa <127966848+vvsisaa@users.noreply.github.com>
2023-05-19 11:48:25 -0300

    Add files via upload

---
+++ isa.py
@@ -0,0 +1,32 @@
+print("ola bella ", end=".")
+print("bella diva")
+
+print("senai")
+
+print("mensagem \"oi")
+
+print(1 + 1)
+
+print("1 x 1 = ", 1* 1)
+
+print("10/ 2 =",10/2)
+
+print("10² =" , 10**2)
+
+print(pow(10,3))
+
+
+nome = ""
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+print(nome)
+
+nome= "A"
+x = 1
+x = 1+1
+y = True
+print(nome)
